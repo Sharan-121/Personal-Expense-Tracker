8b68eb26ee4927d9858af7231c8e83c9fc294aa2
Sharan S S <85382165+Sharan-121@users.noreply.github.com>
2022-01-25 22:21:23 +0530

    Added list view

--- lib/widgets/transactions_list.dart
+++ lib/widgets/transactions_list.dart
@@ -9,47 +9,51 @@ class TransList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transList.map((tx) {
-        return Card(
-            elevation: 5,
-            child: Row(
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.all(10),
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.purple,
-                      width: 2,
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+              elevation: 5,
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    child: Text(
+                      '\₹ ${transList[index].cost}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
                     ),
                   ),
-                  child: Text(
-                    '\₹ ${tx.cost}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.purple,
-                    ),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      tx.title,
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      DateFormat.yMMMd().format(tx.date),
-                      style: TextStyle(color: Colors.grey, fontSize: 16),
-                    )
-                  ],
-                )
-              ],
-            ));
-      }).toList(),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        transList[index].title,
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        DateFormat.yMMMd().format(transList[index].date),
+                        style: TextStyle(color: Colors.grey, fontSize: 16),
+                      )
+                    ],
+                  )
+                ],
+              ));
+        },
+        itemCount: transList.length,
+      ),
     );
   }
 }
