68cbf5cd045e36a84f2030b5018040e44253b956
Sharan S S <85382165+Sharan-121@users.noreply.github.com>
2022-01-27 12:15:43 +0530

    Added list tile

--- lib/widgets/transactions_list.dart
+++ lib/widgets/transactions_list.dart
@@ -10,7 +10,7 @@ class TransList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 500,
+      height: 400,
       child: transList.isEmpty
           ? Column(children: <Widget>[
               Text(
@@ -26,44 +26,40 @@ class TransList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (context, index) {
                 return Card(
-                    elevation: 5,
-                    child: Row(
-                      children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          margin: EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 20),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Theme.of(context).colorScheme.primary,
-                              width: 2,
-                            ),
-                          ),
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      radius: 32,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: FittedBox(
                           child: Text(
                             '\₹ ${transList[index].cost.toStringAsFixed(2)}',
                             style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Theme.of(context).colorScheme.primary,
-                            ),
+                                color: Colors.white,
+                                fontFamily: 'QuickSand',
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold),
                           ),
                         ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              transList[index].title,
-                              style: Theme.of(context).textTheme.headline6,
-                            ),
-                            Text(
-                              DateFormat.yMMMd().format(transList[index].date),
-                              style:
-                                  TextStyle(color: Colors.grey, fontSize: 16),
-                            )
-                          ],
-                        )
-                      ],
-                    ));
+                      ),
+                    ),
+                    title: Text(
+                      transList[index].title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(transList[index].date),
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 16,
+                          fontFamily: 'QuickSand',
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                );
               },
               itemCount: transList.length,
             ),
