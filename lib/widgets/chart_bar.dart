a3b5437122ece8a3d5306b83d234de3cff3c122d
Sharan S S <85382165+Sharan-121@users.noreply.github.com>
2022-01-27 12:14:18 +0530

    fixed chart layout issue

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -12,7 +12,13 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        FittedBox(child: Text('\₹ ${spendingAmount.toStringAsFixed(0)}')),
+        Container(
+          height: 20,
+          child: FittedBox(
+              child: Text(
+            '\₹ ${spendingAmount.toStringAsFixed(0)}',
+          )),
+        ),
         SizedBox(
           height: 4,
         ),
